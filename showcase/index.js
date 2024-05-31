edbb5feffa60f21b9d3657cbc1417c07f6684e7a
illonion <zhou.lawrence.yz@gmail.com>
2024-05-31 22:55:44 +1000

    Add logic for stats and map details

--- showcase/index.js
+++ showcase/index.js
@@ -6,6 +6,10 @@ socket.onclose = event => { console.log("Socket Closed Connection: ", event); so
 socket.onerror = error => { console.log("Socket Error: ", error) }
 
 // Map details
+const titleNameEl = document.getElementById("titleName")
+const artistNameEl = document.getElementById("artistName")
+const mapperNameEl = document.getElementById("mapperName")
+const difficultyNameEl = document.getElementById("difficultyName")
 let currentId, currentMd5
 let foundMapInMappool = false
 // Stats
@@ -20,4 +24,33 @@ const statsODEl = document.getElementById("statsOD")
 socket.onmessage = async (event) => {
     const data = JSON.parse(event.data)   
     console.log(data)
+
+    // Map details
+    if (currentId !== data.menu.bm.id || currentMd5 !== data.menu.bm.md5) {
+        currentId = data.menu.bm.id
+        currentMd5 = data.menu.bm.md5
+
+        titleNameEl.innerText = data.menu.bm.metadata.title
+        artistNameEl.innerText = data.menu.bm.metadata.artist
+        mapperNameEl.innerText = data.menu.bm.metadata.mapper
+        difficultyNameEl.innerText = data.menu.bm.metadata.difficulty
+    }
+    // Stats
+    if (!foundMapInMappool) {
+        // Length
+        let lengthSeconds = Math.round(data.menu.bm.time.full / 1000)
+        let lengthMinutes = Math.floor(lengthSeconds / 60)
+        let remainderSeconds = lengthSeconds % 60
+        statsLengthEl.innerText = `${lengthMinutes}:${("0" + remainderSeconds.toString()).slice(-2)}`
+        // SR
+        statsSREl.innerText = data.menu.bm.stats.fullSR
+        // BPM
+        statsBPMEl.innerText = data.menu.bm.stats.BPM.common
+        // CS
+        statsCSEl.innerText = data.menu.bm.stats.CS
+        // HP
+        statsHPEl.innerText = data.menu.bm.stats.HP
+        // OD
+        statsODEl.innerText = data.menu.bm.stats.OD
+    }
 }
