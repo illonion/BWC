c269d018e719d94adf6b7bf38870fc01f6a11b19
illonion <zhou.lawrence.yz@gmail.com>
2024-06-17 14:39:18 +1000

    Load in sample scores

--- qualifier-results/index.js
+++ qualifier-results/index.js
@@ -1,9 +1,19 @@
 // Load in mappool
 let allBeatmaps
 async function getMappool() {
-    const response = await fetch("../_data/sample_qualifier_results/beatmaps.json")
+    const response = await fetch("../_data/sample-qualifier-results/beatmaps.json")
     const responseJson = await response.json()
     allBeatmaps = responseJson.beatmaps
+    console.log(allBeatmaps)
 }
 getMappool()
 
+// Load in scores
+let allScores
+async function getScores() {
+    const response = await fetch("../_data/sample-qualifier-results/scores.json")
+    const responseJson = await response.json()
+    allScores = responseJson
+    console.log(allScores)
+}
+getScores()
