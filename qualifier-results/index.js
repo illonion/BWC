51e8225407719f907fb147d85a67350143cc84bc
illonion <zhou.lawrence.yz@gmail.com>
2024-06-18 22:30:22 +1000

    Add and load in sample teams

--- qualifier-results/index.js
+++ qualifier-results/index.js
@@ -4,7 +4,6 @@ async function getMappool() {
     const response = await fetch("../_data/sample-qualifier-results/beatmaps.json")
     const responseJson = await response.json()
     allBeatmaps = responseJson.beatmaps
-    console.log(allBeatmaps)
 }
 getMappool()
 
@@ -14,6 +13,15 @@ async function getScores() {
     const response = await fetch("../_data/sample-qualifier-results/scores.json")
     const responseJson = await response.json()
     allScores = responseJson
-    console.log(allScores)
 }
 getScores()
+
+// Load in all teams
+let allTeams
+async function getTeams() {
+    const response = await fetch("../_data/sample-qualifier-results/teams.json")
+    const responseJson = await response.json()
+    allTeams = responseJson
+}
+
+getTeams()
